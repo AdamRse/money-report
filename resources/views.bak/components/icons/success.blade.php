7473043021393c6d3d7a212cd443a137bcba8dd6
AdamRse <bakaarion3@gmail.com>
2025-03-03 15:34:13 +0100

    je vire le .back des vues

--- resources/views.bak/components/icons/success.blade.php
+++
@@ -1,4 +0,0 @@
-<svg viewBox="0 0 24 24" class="alert-icon-svg">
-    <circle cx="12" cy="12" r="11" fill="#4CAF50"/>
-    <path d="M7 13l3 3 7-7" stroke="white" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
