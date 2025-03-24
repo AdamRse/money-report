68358c19823bca27af9976ef2f73d704dd49cfad
AdamRse <bakaarion3@gmail.com>
2025-03-24 10:47:02 +0100

    fix nommage use

--- app/Interfaces/Services/IncomeStatisticsServiceInterface.php
+++ app/Interfaces/Services/IncomeStatisticsServiceInterface.php
@@ -3,7 +3,6 @@
 namespace App\Interfaces\Services;
 
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Support\Collection as SupportCollection;
 
 interface IncomeStatisticsServiceInterface {
     public function calculateStatistics(Collection $incomes): array;
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,13 +2,16 @@
 
 namespace App\Providers;
 
+use App\Interfaces\Repositories\IncomeRepositoryInterface;
 use App\Interfaces\Services\DateParserServiceInterface;
 use App\Interfaces\Services\DocumentParserServiceInterface;
 use App\Interfaces\Services\IncomeDuplicateCheckerServiceInterface;
+use App\Interfaces\Services\IncomeStatisticsServiceInterface;
 use App\Repositories\IncomeRepository;
 use App\Services\DateParserService;
 use App\Services\DocumentParserService;
 use App\Services\IncomeDuplicateCheckerService;
+use App\Services\IncomeStatisticsService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider {
@@ -16,10 +19,11 @@ class AppServiceProvider extends ServiceProvider {
      * Register any application services.
      */
     public function register(): void {
-        $this->app->bind(IncomeRepository::class, IncomeRepository::class);
+        $this->app->bind(IncomeRepositoryInterface::class, IncomeRepository::class);
         $this->app->bind(DateParserServiceInterface::class, DateParserService::class);
         $this->app->bind(IncomeDuplicateCheckerServiceInterface::class, IncomeDuplicateCheckerService::class);
         $this->app->bind(DocumentParserServiceInterface::class, DocumentParserService::class);
+        $this->app->bind(IncomeStatisticsServiceInterface::class, IncomeStatisticsService::class);
     }
 
     /**
